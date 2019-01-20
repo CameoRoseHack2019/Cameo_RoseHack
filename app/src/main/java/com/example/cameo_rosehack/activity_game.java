5a9b915d29fe44ccdbca6816ebc2187fda5fbe0e
jt7898 <32527413+jt7898@users.noreply.github.com>
2019-01-20 07:16:41 -0800

    Update activity_game.java

--- app/src/main/java/com/example/cameo_rosehack/activity_game.java
+++ app/src/main/java/com/example/cameo_rosehack/activity_game.java
@@ -6,6 +6,7 @@ import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import static com.example.cameo_rosehack.state.ACTION;
 import static com.example.cameo_rosehack.state.END;
@@ -168,7 +169,7 @@ public class activity_game extends AppCompatActivity {
 
         switch (S) {                // state Actions
             case INIT:                  // Start state
-                Deck draw = new Deck();
+                final Deck draw = new Deck();
                 draw.shuffleDeck();
                 DealCardsToPlayers(draw);
 
@@ -176,6 +177,15 @@ public class activity_game extends AppCompatActivity {
 
             case PLAYER1:               // PLAYER1 state
 
+                button7.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        List<> tempList = new List<>;
+                        Random randomIndex = new Random();
+                        int randomNumber = randomIndex.nextInt();
+
+                    }
+                });
                 break;
 
             case PLAYER2:               // PLAYER2 state
