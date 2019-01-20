ba2134c95b073b075eb60322ef492ddb017906e8
Mari Hayashi <mhaya010@ucr.edu>
2019-01-19 20:24:53 -0800

    Updated GameScreen

--- app/src/main/java/com/example/cameo_rosehack/activity_game.java
+++ app/src/main/java/com/example/cameo_rosehack/activity_game.java
@@ -14,22 +14,39 @@ public class activity_game extends AppCompatActivity
     private Button middle;
     private TextView text;
 
-    private int[] value_opponent; // like int[0] = 13 if the first card of the opponent is King.
-    private int[] value_player;
 
-
-
+    /*
+    cards:
+    0: jorker
+    1 - 13: heart
+    14 - 26: spade
+    27 - 39: dia
+    40 - 52: club
+    53: jorker
+     */
+    private int[] cards =
+            {
+                    R.drawable.black_joker,
+                    R.drawable.h1, R.drawable.h2, R.drawable.h3, R.drawable.h4, R.drawable.h5, R.drawable.h6, R.drawable.h7,
+                    R.drawable.h8, R.drawable.h9, R.drawable.h10, R.drawable.h11, R.drawable.h12, R.drawable.h13,
+                    R.drawable.s1, R.drawable.s2, R.drawable.s3, R.drawable.s4, R.drawable.s5, R.drawable.s6, R.drawable.s7,
+                    R.drawable.s8, R.drawable.s9, R.drawable.s10, R.drawable.s11, R.drawable.s12, R.drawable.s13,
+                    R.drawable.d1, R.drawable.d2, R.drawable.d3, R.drawable.d4, R.drawable.d5, R.drawable.d6, R.drawable.d7,
+                    R.drawable.d8, R.drawable.d9, R.drawable.d10, R.drawable.d11, R.drawable.d12, R.drawable.d13,
+                    R.drawable.c1, R.drawable.c2, R.drawable.c3, R.drawable.c4, R.drawable.c5, R.drawable.c6, R.drawable.c7,
+                    R.drawable.c8, R.drawable.c9, R.drawable.c10, R.drawable.c11, R.drawable.h12, R.drawable.c13,
+                    R.drawable.red_joker
+            };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
 
         middle = findViewById(R.id.button_middle);
         text = findViewById(R.id.text);
 
-       Button[] cards_opponent = {
+        Button[] cards_opponent = {
                 (Button) findViewById(R.id.button_c1_opp),
                 (Button) findViewById(R.id.button_c2_opp),
                 (Button) findViewById(R.id.button_c3_opp),
@@ -50,23 +67,12 @@ public class activity_game extends AppCompatActivity
                 (Button) findViewById(R.id.button_c7),
                 (Button) findViewById(R.id.button_c8)
         };
-
-        cards_player[0].setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view) {
-                test();
-            }
-
-        });
     }
 
-    protected void test()
-    {
-        middle.setBackgroundResource(R.drawable.card); // idk how to change background :(
-    }
     protected void drawCard(int cardNum)
     {
         text.setVisibility(View.INVISIBLE);
         middle.setVisibility(View.VISIBLE);
-        //middle.setBackgroundResource(R.drawable."dia7.png"); // idk how to change background :(
+        middle.setBackgroundResource(cards[cardNum]);
     }
 }
